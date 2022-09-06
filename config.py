ae9e2f23f09b37efadc983b7c999d92aa1e45f86
damski_ugod.nuk <ponomarenkonikita03@gmail.com>
2022-09-06 16:44:58 +0300

    db maintenance

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "5609125980:AAH7iwUCoe-Yu0xeE2f3gqwLEuq8sMZVHOQ"
 APP_URL = "https://dance-dreamer.herokuapp.com/" + BOT_TOKEN
-DB_URI = "postgres://rybdppmahajmla:5bcabbf74a4f808dee9bc4a01f505222a9f85d9bdd91ffe5fcc7cfb46751edeb@ec2-54-228-125-183.eu-west-1.compute.amazonaws.com:5432/d3koq0gfsj6agi"
+DB_URI = "postgres://kgfzpoamidthie:6ee3fce1165d0fc4fbde298c54a96fa070fdced9c8056379dab680c0d3e72341@ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/d2dkpj9hscmctp"
