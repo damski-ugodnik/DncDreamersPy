1c08b02a6a3ab52f152056695dd9e0376281a302
damski_ugod.nuk <ponomarenkonikita03@gmail.com>
2022-10-04 15:48:44 +0300

    fuuuck

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 BOT_TOKEN = "5609125980:AAH7iwUCoe-Yu0xeE2f3gqwLEuq8sMZVHOQ"
 APP_URL = "https://dance-dreamer.herokuapp.com/" + BOT_TOKEN
+
 DB_URI = "postgres://jeapnlykfhgrhs:19acb86cf78b6939c24b4cdb298035e0ab31ca4a0d3225a030056217d441dcbf@ec2-54-228-201-167.eu-west-1.compute.amazonaws.com:5432/d4gvgagulbr0q4"
